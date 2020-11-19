5a8b36155ee535f71d1d34021383c08f5def7554
LiamOSullivan <lmosulli@tcd.ie>
2020-11-19 12:44:57 +0000

    working loop behaviour

--- ftp-fetch-and-save.js
+++ ftp-fetch-and-save.js
@@ -1,11 +1,11 @@
 const ftp = require('basic-ftp')
 
-const REMOTE_PATH = 'public_html/moved_1/m50/m501464535501.json'
-const LOCAL_PATH = 'data/moved_1/m50/m501464535501.json'
+const REMOTE_PATH = 'public_html/moved_1/m50/'
+const LOCAL_PATH = '../cork-data-archive-0⁩/⁨moved_1⁩/m50/'
 
 example()
 
-async function example() {
+async function example () {
   const client = new ftp.Client()
   //   console.log(client)
   //   client.ftp.verbose = true
@@ -16,9 +16,28 @@ async function example() {
       password: 'Hp$sVEooKQ%o',
       secure: false
     })
-    // console.log(await client.list())
-    // await client.uploadFrom('README.md', 'README_FTP.md')
-    await client.downloadTo(LOCAL_PATH, REMOTE_PATH)
+    await client.cd(REMOTE_PATH)
+    console.log(await client.pwd())
+    for (let i = 0; i < 10; i += 1) {
+      await client.cd('..')
+      console.log(await client.pwd())
+      console.log('*****')
+      await client.cd('m50')
+      console.log(await client.pwd())
+      console.log('Loop ' + i)
+      const start = new Date()
+      console.log('Started at ' + start)
+      // start = start.getTime()
+      await client.downloadToDir(LOCAL_PATH)
+      console.log('Finished download dir contents')
+      const downloaded = new Date()
+      // downloaded = downloaded.getTime()
+      // const elapsed = (downloaded - start) / 1000
+      // console.log('Download duration (s):' + elapsed)
+      await client.clearWorkingDir()
+      console.log('Cleared working dir')
+      console.log('*****')
+    }
   } catch (err) {
     console.log(err)
   }
