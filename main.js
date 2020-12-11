fe764e3f355431052c8d0e360bc40c703d09ddb1
LiamOSullivan <lmosulli@tcd.ie>
2020-12-11 16:05:08 +0000

    clean

---
+++ main.js
@@ -0,0 +1,52 @@
+const ftp = require('basic-ftp')
+
+const REMOTE_PATH = 'public_html/cork/waterLevels/'
+const LOCAL_PATH = '../cork-data-archive-1/waterLevels/'
+
+example()
+
+async function example () {
+  for (let i = 0; i < 10; i += 1) {
+    const client = new ftp.Client()
+    //   console.log(client)
+    //   client.ftp.verbose = true
+    try {
+      await client.access({
+        host: 'cphost1.nuim.ie',
+        user: 'corkdashboard',
+        password: 'Hp$sVEooKQ%o',
+        secure: false
+      })
+      console.log('*****')
+      console.log('*   *')
+      console.log('*   *')
+      await client.cd(REMOTE_PATH)
+      console.log(await client.pwd())
+      // for (let i = 0; i < 10; i += 1) {
+      // await client.cd('..')
+      // console.log(await client.pwd())
+      // await client.cd('alerts')
+      // console.log(await client.pwd())
+      console.log('Loop ' + i)
+      let date = new Date()
+      console.log('Started at ' + date)
+      await client.downloadToDir(LOCAL_PATH)
+      date = new Date()
+      console.log('Finished download dir contents at ' + date)
+      console.log('*****')
+      await client.clearWorkingDir()
+      console.log('Cleared working dir')
+      console.log('*****')
+      date = new Date()
+      console.log('Finished at ' + date)
+      console.log('*   *')
+      console.log('*   *')
+      console.log('*****')
+      // }
+    } catch (err) {
+      console.log(err)
+      client.close()
+    }
+    client.close()
+  }
+}
